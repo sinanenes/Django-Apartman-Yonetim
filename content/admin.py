8bf8043568f1cd46b9d38de69b44ae1102351271
sinanenes <sinanenes86@gmail.com>
2022-04-11 02:41:46 +0300

    Imagen tablosu icin gerekli model olusturuldu, content altina register edildi, bagli oldugu content tablosunun objesi olusturulurken ayni anda ona ait imagen objelerini de ekleyebilmek icin inline property olusturulup contentadmin sinifina setlendi.

--- content/admin.py
+++ content/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from content.models import Menu, Content
+from content.models import Menu, Content, Imagen
+
+
+class ContentImagenInline(admin.TabularInline):
+    model = Imagen
+    extra = 5
 
 
 class MenuAdmin(admin.ModelAdmin):
@@ -12,7 +17,14 @@ class MenuAdmin(admin.ModelAdmin):
 class ContentAdmin(admin.ModelAdmin):
     list_display = ['title', 'menu', 'type', 'status']
     list_filter = ['status', 'menu']
+    inlines = [ContentImagenInline]
+
+
+class ImagenAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content', 'image']
+    list_filter = ['content']
 
 
 admin.site.register(Menu, MenuAdmin)
 admin.site.register(Content, ContentAdmin)
+admin.site.register(Imagen, ImagenAdmin)
--- content/models.py
+++ content/models.py
@@ -8,7 +8,7 @@ class Menu(models.Model):
         ('False', 'Hayır')
     )
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -33,7 +33,7 @@ class Content(models.Model):
         ('Survey', 'Anket')
     )
 
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=150)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -46,6 +46,15 @@ class Content(models.Model):
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Imagen(models.Model):
+    content = models.ForeignKey(Content, on_delete=models.CASCADE)  # relation with Content table
+    title = models.CharField(max_length=50)
+    image = models.ImageField(blank=True, upload_to='images/')
 
     def __str__(self):
         return self.title
