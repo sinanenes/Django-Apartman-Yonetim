314becf3fa2099a8f10baa2b29eb19e30cbb083f
sinanenes <sinanenes86@gmail.com>
2022-04-16 13:53:24 +0300

    setting modeli olusturuldu, sitenin title, favicon gibi ayarlari adminden girilip site uzerinde dinamik olarak gosterimi saglandi

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from home.models import Setting
+
+
 def index(request):
-    text = "Merhaba Django (context)!!! <br> Django Kurulumu: python -m pip install Django <br> Proje Olusturma: " \
-           "django-admin startproject mysite <br> App Ekleme: python manage.py startapp polls"
-    context = {'text': text}
+    # setting = Setting.objects.all()
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
     return render(request, 'index.html', context)
-    # return HttpResponse("Su anda home_views.py index metodundan ilk karsilama mesajini gormektesiniz: %s" % text)
