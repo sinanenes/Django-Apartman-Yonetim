f0e0d037e81e0071334405d580c69c98a3ef699e
sinanenes <sinanenes86@gmail.com>
2022-05-01 15:05:21 +0300

    slider sayfasi icin duzenleme yapildi, dbden verinin cekilerek sliderda gosterilmesi saglandi

--- home/views.py
+++ home/views.py
@@ -3,13 +3,17 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from content.models import Content
 from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
     # setting = Setting.objects.all()
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, 'page': 'home'}
+    sliderdata = Content.objects.all()[:4]
+    context = {'setting': setting,
+               'page': 'home',
+               'sliderdata': sliderdata}
     return render(request, 'index.html', context)
 
 
