c1439a513048fc30d56318ca10300698cfa5a286
sinanenes <sinanenes86@gmail.com>
2022-05-05 16:54:09 +0300

    tree view of menus for admin panel -> contentlerin bagli oldugu menulerin ve onlarin ust menulerinin tree seklinde admin paneli uzerinde gosterimi saglandi

--- content/admin.py
+++ content/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from mptt.admin import MPTTModelAdmin, DraggableMPTTAdmin
+
 from content.models import Menu, Content, Imagen
 
 
@@ -28,6 +30,44 @@ class ImagenAdmin(admin.ModelAdmin):
     readonly_fields = ('image_tag',)
 
 
-admin.site.register(Menu, MenuAdmin)
+class CategoryAdminTree(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title',
+                    'related_contents_count', 'related_contents_cumulative_count')
+    list_display_links = ('indented_title',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative product count
+        qs = Menu.objects.add_related_count(
+            qs,
+            Content,
+            'menu',
+            'contents_cumulative_count',
+            cumulative=True)
+
+        # Add non cumulative product count
+        qs = Menu.objects.add_related_count(qs,
+                                            Content,
+                                            'menu',
+                                            'contents_count',
+                                            cumulative=False)
+        return qs
+
+    def related_contents_count(self, instance):
+        return instance.contents_count
+
+    related_contents_count.short_description = 'Related contents (for this specific category)'
+
+    def related_contents_cumulative_count(self, instance):
+        return instance.contents_cumulative_count
+
+    related_contents_cumulative_count.short_description = 'Related contents (in tree)'
+
+
+# admin.site.register(Menu, MenuAdmin)
+# admin.site.register(Menu, MPTTModelAdmin)
+admin.site.register(Menu, CategoryAdminTree)
 admin.site.register(Content, ContentAdmin)
 admin.site.register(Imagen, ImagenAdmin)
